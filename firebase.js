cbbfdc1921500be4846d65597aeb1cce56bf15ed
Esthererani1234 <esthererani14@gmail.com>
2025-05-09 18:07:12 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,8 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDElIYW5-gZGJTH--ORVD1xKyjeRyJh96c",
+  apiKey: "AIzaSyBtbaGNzL0Pi4T39_tTa_jSUcKinKL0NBc", // your new key
   authDomain: "workedin-38aa3.firebaseapp.com",
   projectId: "workedin-38aa3",
   storageBucket: "workedin-38aa3.appspot.com",
   messagingSenderId: "305634639635",
-  appId: "1:305634639635:web:9350cc6235e83064f424e6"
+  appId: "1:305634639635:web:xxxxx" // leave unchanged
 };
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
