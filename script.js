8dfbc54f033387004cf31c491b9527457caf20e2
Esthererani1234 <esthererani14@gmail.com>
2025-05-10 21:47:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('WorkedIn is live');
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const searchBox = document.querySelector(".search-box");
+
+  if (searchBox) {
+    searchBox.addEventListener("input", (e) => {
+      const query = e.target.value.toLowerCase();
+      // In the future, filter real products here
+      console.log("Searching for:", query);
+    });
+  }
+
+  console.log("Homepage script loaded");
+});
